120a3a7092a778d1cf52899469ca295430d49a7d
Hunter1us <pofofwar@gmail.com>
2018-09-08 14:02:29 +0300

    08.09.18

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
   axios({
     method: 'get',
     crossDomain: true,
-    url: 'https://api.vk.com/method/groups.get?user_id=70281897&access_token=14d5129446eaf1c8c73e72c47d8403e972bce699ad7d2f781eb97663267c9f35d4c9756acbf7c4beed97d&v=5.84',
+    url: 'https://api.vk.com/method/groups.get?user_id=70281897&access_token=(IN TXT)&v=5.84',
   }).then((responce) => {
     res.status(200).send(JSON.stringify(responce.data.response.items));
   }).catch((err) => {
